c54d8f5f39c57438fe0a497afede21b0969ef28f
nimbrethil81 <stingyfit@gmail.com>
2026-06-24 10:41:46 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -30,7 +30,7 @@
 
 // ─── CONFIG ──────────────────────────────────────────────────────────────────
 
-const CACHE_VERSION  = 'wbc-v1';
+const CACHE_VERSION  = 'wbc-v2';
 const CACHE_SHELL    = CACHE_VERSION + '-shell';   // app shell + data
 const CACHE_FONTS    = CACHE_VERSION + '-fonts';   // Google Fonts responses
 
